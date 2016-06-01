87f204b52eb9e996c50df62c347c5ad9f16b7b3e
Maxwell Renke <maxrenke@gmail.com>
2016-05-31 20:44:34 -0400

    added game and viewers

--- twitch.py
+++ twitch.py
@@ -12,10 +12,13 @@ for i in follows:
 	n_data = json.loads(n_response.read())
 	n_string = n_data['stream']
 	if not (n_string==None):
-		print "[",count,"] ",name
+		n_viewers = n_string['viewers']
+		n_game = n_string['game']
+		print "[",count,"] ",name,"-",n_game,"-",n_viewers
 		names.append(name)
 		count = count + 1
 selection = raw_input("Select stream(s): ")
 words = selection.split()
 for w in words:
 	subprocess.call('start ls ' + names[int(w)-1] + ' best', shell=True)
+
