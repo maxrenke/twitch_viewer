0df5a856b5e96338ada6d0ead57ddf620c36a242
Maxwell Renke <maxrenke@gmail.com>
2018-06-17 16:07:52 -0700

    update twitch.py
    add the top 25 twitch streams to the list of streams to choose from

--- twitch.py
+++ twitch.py
@@ -1,5 +1,5 @@
 import urllib, json, os, subprocess, string
-url = "https://api.twitch.tv/kraken/users/[USERNAME]/follows/channels?limit=100?oauth_token=oauth:[OAUTH_TOKEN]&client_id=[CLIENT_ID]"
+url = "https://api.twitch.tv/kraken/users/[USERNAME]/follows/channels?limit=100?client_id=[CLIENT_ID]"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
 follows = data['follows']
@@ -22,6 +22,29 @@ for i in follows:
 				print "[",count,"] ",name,"-",filtered_n_title,"-",n_game,"-",n_viewers
 				names.append(name)
 				count = count + 1
+				
+url = "https://api.twitch.tv/kraken/streams?limit=25&client_id=[CLIENT_ID]"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+streams = data['streams']
+for i in streams:
+	name = i['channel']['name']
+	n_url = "https://api.twitch.tv/kraken/streams/" + name + "?client_id=[CLIENT_ID]"
+	n_response = urllib.urlopen(n_url)
+	n_data = json.loads(n_response.read())
+	n_string = n_data['stream']
+	if not (n_string==None):
+		n_viewers = n_string['viewers']
+		n_game = n_string['game']
+		n_channel = n_string['channel']
+		n_title = n_channel['status']
+		filtered_n_title = filter(lambda x: x in string.printable, n_title)
+		if (n_string['stream_type']=='live'):
+			if 'rerun' not in filtered_n_title.lower():
+				print "[",count,"] ",name,"-",filtered_n_title,"-",n_game,"-",n_viewers
+				names.append(name)
+				count = count + 1
+
 selection = raw_input("Select stream(s): ")
 words = selection.split()
 wordString = "/"
